bbc900b62475a55c976725a1cb69bd754af6c6a8
hilary <c011605154@edu.teu.ac.jp>
2020-04-02 22:42:29 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 'use strict';
-const number = process.argv[2] || 0;
-let sum = 0;
-for (let i = 1; i <= number; i++) {
-    sum = sum + i;
+
+function fib(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1
+    }
+    return fib(n - 1) + fib(n - 2)
 }
-console.log(sum);
+
+// #01
+// const number = process.argv[2] || 0;
+// let sum = 0;
+// for (let i = 1; i <= number; i++) {
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+
+// #02
+const length = 30;
+for (let i = 0; i < length; i++) {
+    console.log(fib(i))
+}
